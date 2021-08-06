4b8f54b6bdaf2e58366f20bb5a7d7d7bd9a2e281
npecka <zangoose107@gmail.com>
2021-08-05 20:59:12 -0500

    Create function consumer_create and altered some other functions. Setup main loop to ask users for params for producer or consumer. The producer.py is called containing the functions to create producers and consumers. It then either producers user specified messages or consumes what already exists in the targeted kafka cluster
    Signed-off-by: npecka <zangoose107@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 
 from producer import producer_create
 from producer import p_create
+from producer import consumer_create
 from producer import c_create
 
 if __name__ == "__main__":
@@ -12,23 +13,36 @@ if __name__ == "__main__":
 
         val = input("Would you like to produce or consume (enter 'p' or 'c'): ")
         if val == "p" or val == "c":
+            use_ssl = input("Does the bootstrap use SASL/SSL? (enter 'yes' or 'no'): ")
             bootstrap_servers = input("Please input bootstrap servers (ex. localhost:9092): ")
             client_id = input("Please input a client id: ")
-            security_protocol = input("Enter the security protocl (ex. PLAIN_SASL: ")
-            sasl_mechanism = input("Enter the sasl mechanism (ex. PLAINTEXT)")
-            sasl_plain_username = input("Enter a username: ")
-            sasl_plain_password = input("Enter a password: ")
-            ssl_context = input("Enter an ssl context: ")
             topic = input("Enter a topic name: ")
-            producer = producer_create(bootstrap_servers, client_id, security_protocol, sasl_mechanism,
-                                    sasl_plain_username, sasl_plain_password, ssl_context);
+            if use_ssl == "yes":
+                security_protocol = input("Enter the security protocol (ex. PLAIN_SASL): ")
+                sasl_mechanism = input("Enter the sasl mechanism (ex. PLAINTEXT): ")
+                sasl_plain_username = input("Enter a username: ")
+                sasl_plain_password = input("Enter a password: ")
+                ssl_context = input("Enter an ssl context: ")
+                if val == "p":
+                    producer = producer_create(bootstrap_servers, client_id, security_protocol, sasl_mechanism,
+                                               sasl_plain_username, sasl_plain_password, ssl_context)
+                else:
+                    consumer_create(topic, bootstrap_servers, client_id, security_protocol,
+                                    sasl_mechanism, sasl_plain_username, sasl_plain_password,
+                                    ssl_context)
+                    t1 = threading.Thread(c_create(consumer))
+                    t1.start()
+            else:
+                if val == "p":
+                    producer = producer_create(bootstrap_servers, client_id)
             if val == "p":
-                num_of_messages = input("Enter # of messages wanting to be sent")
+                num_of_messages = input("Enter # of messages wanting to be sent: ")
+                num_of_messages = int(num_of_messages)
                 t1 = threading.Thread(p_create(producer, topic, num_of_messages))
                 t1.start()
-            else:
-                t1 = threading.Thread(c_create(topic, bootstrap_servers, client_id, security_protocol, sasl_mechanism,
-                                    sasl_plain_username, sasl_plain_password, ssl_context))
+            if val == "c" and use_ssl != "yes":
+                consumer = consumer_create(topic, bootstrap_servers, client_id)
+                t1 = threading.Thread(c_create(consumer))
                 t1.start()
         else:
             print("Incorrect value for producer or consumer")
--- producer.py
+++ producer.py
@@ -8,7 +8,7 @@ context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
 
 
-def producer_create(bootstrap_servers="", client_id="", security_protocol="",
+def producer_create(bootstrap_servers="", client_id="", security_protocol="PLAINTEXT",
                     sasl_mechanism="", sasl_plain_username="",
                     sasl_plain_password="", ssl_context=context):
     producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
@@ -21,25 +21,31 @@ def producer_create(bootstrap_servers="", client_id="", security_protocol="",
                              )
     return producer
 
+
 def p_create(thread_name, topic_name, num_of_messages):
     for j in range(1, num_of_messages):
         thread_name.send(topic_name, b'%d' % j)
         print(j)
 
-def c_create(bootstrap_servers="", client_id="", security_protocol="",
+def consumer_create(topic="", bootstrap_servers="", client_id="", security_protocol="PLAINTEXT",
                     sasl_mechanism="", sasl_plain_username="",
-                    sasl_plain_password="", ssl_context=context,
-                    topic=""):
-    thread_name = KafkaConsumer(topic,
-                               bootstrap_servers=bootstrap_servers,
-                               client_id=client_id,
-                               security_protocol=security_protocol,
-                               sasl_mechanism=sasl_mechanism,
-                               sasl_plain_username=sasl_plain_username,
-                               sasl_plain_password=sasl_plain_password,
-                               ssl_context=ssl_context,
-                               consumer_timeout_ms=10000)
+                    sasl_plain_password="", ssl_context=context):
+    consumer = KafkaConsumer(topic,
+                             group_id=client_id,
+                             bootstrap_servers=[bootstrap_servers],
+                             security_protocol=security_protocol,
+                             sasl_mechanism=sasl_mechanism,
+                             sasl_plain_username=sasl_plain_username,
+                             sasl_plain_password=sasl_plain_password,
+                             ssl_context=ssl_context,
+                             consumer_timeout_ms=10000,
+                             auto_offset_reset='earliest',
+                             enable_auto_commit=False
+                             )
+    return consumer
 
+
+def c_create(thread_name):
     for message in thread_name:
         print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                              message.offset, message.key,
